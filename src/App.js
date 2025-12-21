03d2a983206b4509efd033d9157c74cec4d48529
wes10645 <wes10645@gmail.com>
2025-12-20 20:33:21 -0500

    Tried adding error handling logic

--- src/App.js
+++ src/App.js
@@ -3,32 +3,48 @@ import {useState, useEffect} from "react"; //importing useState, which stores da
 function App(){ //sets empty arrays for artist, 
   const[artists, setArtists]=useState([]); 
   const[loading, setLoading]=useState(true); //sets state for of waiting for data
-
+  const[error, setError] = useState(null);
 
 
   useEffect(()=>{
-     fetch("https://jsonplaceholder.typicode.com/users") //supposed to be a. placeholder for now
-      .then((response) => response.json())
-      .then((data) => {
-        const names = data.map((user) => user.name);
-        setArtists(names);
-        setLoading(false);//not loading anymore, stop loading screen
-      });
-  }, []);//runs effect only one time 
+    async function fetchArtists() {
+      try{
+        const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+        );
+        if (!response.ok){
+          throw new Error ("failed to fetch your stats:(");
+        }
+      const data = await response.json();
+      const names = data.map((user) => user.name);
+
+      setArtists(names);
+    }catch (err){
+        setError(err.message);
+    }finally{
+      setLoading(false);
+    }
+    }
+
+    fetchArtists();
+  },[]);
 return (
   <div>
     <h1>Welcome To Wesley's Spotify Listening Stats!!!</h1>
 
-      {loading ?(
-        <p>trying to load some stats....</p>
-      ):(
-        <ul>
-          {artists.map((artist)=>(
-            <li key= {artist}>{artist}</li>
+      {loading&&<p>trying to load some stats...</p>}
+
+      {error&&<p style = {{color: "red"}}>{error}</p>}
+         
+         {!loading &&!error&&(
+          <ul>
+          {artists.map((artist) => (
+            <li key={artist}>{artist}</li>
           ))}
-          </ul>
-          )}
-  </div>
-);
+        </ul>
+      )}
+    </div>
+  );
 }
+
 export default App;
