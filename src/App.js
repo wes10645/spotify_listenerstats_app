8f055ab12e8c77e242643ec012219641c6b6a6a2
wes10645 <wes10645@gmail.com>
2025-12-20 19:38:04 -0500

    started using useEffect, adding useState for loading and artists

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-function App() {
-  return (
-    <div>
-      <h1>Spotify Listening Stats</h1>
-      <p>HEYYYY YENN!!!!this is me messing around</p>
-    </div>
-  );
-}
+import {useState, useEffect} from react; //importing useState, which stores data and useEffect which loads data
 
-export default App;
+function App(){ //sets empty arrays for artist, 
+  const[artists, setArtists]=useState([]); 
+  const[loading, setLoading]=useState(true); //sets state for of waiting for data
+
+
+
+  useEffect(()=>{
+     fetch("https://jsonplaceholder.typicode.com/users") //supposed to be a. placeholder for now
+      .then((response) => response.json())
+      .then((data) => {
+        const names = data.map((user) => user.name);
+        setArtists(names);
+        setLoading(false);//not loading anymore, stop loading screen
+      });
+  }, []);//runs effect only one time 
+}
