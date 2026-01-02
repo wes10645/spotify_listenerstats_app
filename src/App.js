1efe5b56423264fdf8cc7f3c8ae5ab2dbdfba83b
wes10645 <wes10645@gmail.com>
2026-01-02 13:14:35 -0500

    trying to add the spotify button, fix a few small bugs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {useState, useEffect} from "react"; //importing useState, which stores da
 
 
 //im starting to implement the authorization for spotify 
-const CLIENT_ID = "PASTE_YOUR_CLIENT_ID_HERE";
+const CLIENT_ID = "f7ad09fd2de94e8cb33658dd53dafd3d"; //pasted the client id
 const REDIRECT_URI = "http://127.0.0.1:3000";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
@@ -46,7 +46,7 @@ function App(){ //sets empty arrays for artist,
         "https://api.spotify.com/v1/me/top/artists",
         {
           headers:{
-            Authorization: 'Bearer ${accessToken}', 
+            Authorization: `Bearer ${accessToken}`, 
           },
         }
         );
@@ -54,7 +54,7 @@ function App(){ //sets empty arrays for artist,
           throw new Error ("failed to fetch your stats:(");
         }
       const data = await response.json();
-      const names = data.map((user) => user.name);
+      const names = data.items.map((user) => user.name); // fixing data.map to data.items, as spotify returns {items:...}
 
       setArtists(names);
     }catch (err){
@@ -64,11 +64,21 @@ function App(){ //sets empty arrays for artist,
     }
     }
     useEffect(()=>{
-    fetchArtists();
-  },[]);
+    if(accessToken) fetchArtists(); // now only fetches artist after access token is validated
+  },[accessToken]);
+    const loginUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent( //now we add the redirect url 
+    REDIRECT_URI
+  )}&response_type=${RESPONSE_TYPE}&scope=${encodeURIComponent(
+    SCOPES.join(" ")
+  )}`;
 return (
   <div>
     <h1>Welcome To Wesley's Spotify Listening Stats!!!</h1>
+       {!accessToken && (  //checks if already logged in and shows log in button for spotify
+        <a href={loginUrl}>
+        <button>Log in with Spotify</button>
+        </a>
+        )}
 
       {loading&&<p>trying to load some stats...</p>}
 
